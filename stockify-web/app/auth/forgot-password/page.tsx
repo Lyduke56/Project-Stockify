c79db4c5551b577f8530b6273d56e24a2ce1debf
Benideck Longakit <longakitbenideck@gmail.com>
2026-04-01 16:26:02 +0800

    Update page.tsx

---
+++ stockify-web/app/auth/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import NavbarLandingPage from "@/components/navbars/navbar-landing-page";
+
+export default function ForgotPassword() {
+  return (
+    <div className="w-full min-h-screen bg-[#385E31]">
+
+      <NavbarLandingPage />
+      <p>Forgot Password</p>
+    </div>
+  );
+}
