7b4adaf04e3c6dc91999295effc34a6163447ce9
Lyduke56 <rosalclydejustine@gmail.com>
2026-04-08 18:48:50 +0800

    [Feature][Rosal] - admin dashboard
    - added status, to be tweaked

--- stockify-web/components/sections/admin/dashboard-home.tsx
+++ stockify-web/components/sections/admin/dashboard-home.tsx
@@ -1,3 +1,8 @@
+import React from 'react';
+
+import StatCard from "@/components/cards/stat-cards";
+import ShopStatus from "@/components/cards/admin/shop-status";
+
 export default function DashboardHome() {
   return (
     <>
@@ -14,11 +19,47 @@ export default function DashboardHome() {
         </h2>
         <p className="text-stone-400 font-medium">Shop Name Corporation, Inc.</p>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4">
-          <div className="h-40 bg-[#385E31] rounded-xl shadow-lg border-b-8 border-[#F7B71D]" />
-          <div className="h-40 bg-[#385E31] rounded-xl shadow-lg border-b-8 border-[#F7B71D]" />
-          <div className="h-40 bg-[#385E31] rounded-xl shadow-lg border-b-8 border-[#F7B71D]" />
+        <div className="flex justify-center">
+          <div className="justify-center align-center grid grid-cols-1 md:grid-cols-3 gap-4 max-w-3xl">
+          <StatCard 
+           title="Active New Customers"
+           value="58"
+           trendText="↑ 12% from last month"
+           svgName="SalesIcon"
+           icon="📊"
+           className = ""
+          />
+          <StatCard 
+           title="Monthly Revenue"
+           value="62.3k"
+           trendText="↑ 5% from last month"
+           svgName="CustomersIcon"
+           icon="👥"
+           className = ""
+          />
+          <StatCard 
+           title="Total Orders"
+           value="143"
+           trendText="↓ 2% from last month"
+           svgName="ProductsIcon" 
+           icon="📦"
+           className = ""
+          />
+          </div>
         </div>
+
+        <div className="w-full h-[3px] bg-lime-900/50 rounded-full" />
+        
+        <ShopStatus
+          shopName="Coffee Shop"
+          clientName="Shop Name"
+          itemCount={245}
+          lowStockCount={12}
+          revenue="$48.5K"
+          orders={124}
+          onManageShop={() => console.log("manage shop clicked")}
+        />
+
       </div>
     </>
   );
