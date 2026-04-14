009e3c9215bd6f9b4ea47791f682325f1e0367b8
Elle Gabrielle Bernante <ellegabriellebernante@gmail.com>
2026-04-15 00:42:04 +0800

    [Feature][Bernante] Adjusted Header Placements
    Containers will be adjusted accordingly.

--- stockify-web/app/stockify-client-side/profile-settings/page.tsx
+++ stockify-web/app/stockify-client-side/profile-settings/page.tsx
@@ -17,10 +17,10 @@ export default function ClientProfileSettings() {
           {/* TAB HEADER */}
           <section className="w-full h-12 inline-flex flex-col justify-start items-start gap-[3.23px]">
             <div className="self-stretch h-7 relative">
-              <div className="left-0 top-[-1.62px] absolute justify-start text-lime-800 text-2xl font-bold font-['Inter'] leading-7">Profile </div>
+              <div className="left-5 top-[-1.62px] absolute justify-start text-lime-800 text-2xl font-bold font-['Inter'] leading-7">Profile </div>
             </div>
             <div className="self-stretch h-5 relative">
-              <div className="left-0 top-[-1.62px] absolute justify-start text-lime-800/70 text-xs font-normal font-['Inter'] leading-5">Manage and update client and business information</div>
+              <div className="left-5 top-[-1.62px] absolute justify-start text-lime-800/70 text-xs font-normal font-['Inter'] leading-5">Manage and update client and business information</div>
             </div>
           </section>
 
